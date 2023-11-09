9a58476f50a3a07d5795e05c7b08013a8ab9381c
Abin Singh K <141057162+AbinSinghK@users.noreply.github.com>
2023-11-09 00:11:27 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "api_key" {
+  default     = "NRAK-QIJ2YKGXOHCSL5CTY763K8FC13B"
+}
+
+variable "account_id" {
+  default     = "4197589"
+}
+
+variable "region" {
+  default     = "US"
+}
+~                                                                                                                        ~              
